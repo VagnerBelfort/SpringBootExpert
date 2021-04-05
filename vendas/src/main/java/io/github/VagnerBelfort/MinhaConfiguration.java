d27eb1aa303bb37b44674cb901c3314673ed0102
Vagner Belfort <vagner.belfort@hartbgroup.com.br>
2021-04-05 00:33:50 -0300

    Configurations e Beans

---
+++ vendas/src/main/java/io/github/VagnerBelfort/MinhaConfiguration.java
@@ -0,0 +1,13 @@
+package io.github.VagnerBelfort;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinhaConfiguration {
+
+    @Bean(name = "applicationName")
+    public String applicationName(){
+        return "sistema de vendas";
+    }
+}
