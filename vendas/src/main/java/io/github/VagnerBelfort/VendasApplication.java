273d04cb322b6fe039e9e0b837678b9aa81b8f08
Vagner Belfort <vagner.belfort@hartbgroup.com.br>
2021-04-06 00:07:21 -0300

    Salvando e recuperando clientes

--- vendas/src/main/java/io/github/VagnerBelfort/VendasApplication.java
+++ vendas/src/main/java/io/github/VagnerBelfort/VendasApplication.java
@@ -1,14 +1,33 @@
 package io.github.VagnerBelfort;
 
 
+import io.github.VagnerBelfort.domain.entity.Cliente;
+import io.github.VagnerBelfort.domain.repositorio.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes){
+        return args -> {
+            clientes.salvar(new Cliente( "vagner"));
+            clientes.salvar(new Cliente( "belfort"));
+
+            List<Cliente> todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
