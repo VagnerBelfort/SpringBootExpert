5ecd08bdeecf9b5158d621e7b33f139f8c92d745
Vagner Belfort <vagner.belfort@hartbgroup.com.br>
2021-04-09 23:22:23 -0300

    ADD: query methods

--- vendas/src/main/java/io/github/VagnerBelfort/VendasApplication.java
+++ vendas/src/main/java/io/github/VagnerBelfort/VendasApplication.java
@@ -21,32 +21,9 @@ public class VendasApplication {
             clientes.save(new Cliente("vagner"));
             clientes.save(new Cliente("Outro Cliente"));
 
-            List<Cliente> todosClientes = clientes.findAll();
-            todosClientes.forEach(System.out::println);
+            boolean existe = clientes.existsByNome("vagner");
+            System.out.println(existe);
 
-            System.out.println("Atualizando clientes");
-            todosClientes.forEach(c -> {
-                c.setNome(c.getNome() + " atualizado.");
-                clientes.save(c);
-            });
-
-            todosClientes = clientes.findAll();
-            todosClientes.forEach(System.out::println);
-
-            System.out.println("Buscando clientes");
-            clientes.findByNomeLike("Cli").forEach(System.out::println);
-
-            System.out.println("deletando clientes");
-            clientes.findAll().forEach(c -> {
-                clientes.delete(c);
-            });
-
-            todosClientes = clientes.findAll();
-            if(todosClientes.isEmpty()){
-                System.out.println("Nenhum cliente encontrado.");
-            }else{
-                todosClientes.forEach(System.out::println);
-            }
         };
     }
 
