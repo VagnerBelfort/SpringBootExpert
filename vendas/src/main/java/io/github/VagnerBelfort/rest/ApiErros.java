83aa5c0098fed39739c20e0dfdda0db32d2dd978
Vagner Belfort <vagner.belfort@hartbgroup.com.br>
2021-04-21 23:24:57 -0300

    Tratamento de erros

---
+++ vendas/src/main/java/io/github/VagnerBelfort/rest/ApiErros.java
@@ -0,0 +1,17 @@
+package io.github.VagnerBelfort.rest;
+
+import lombok.Data;
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ApiErros {
+
+    @Getter
+    private List<String> errors;
+
+    public ApiErros(String mensagemErro){
+        this.errors = Arrays.asList(mensagemErro);
+    }
+}
