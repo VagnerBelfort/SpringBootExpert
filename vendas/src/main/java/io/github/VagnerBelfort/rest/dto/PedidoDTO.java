519de504f7eb30f2597e4842b1aca442b7250e3e
Vagner Belfort <vagner.belfort@hartbgroup.com.br>
2021-04-24 09:53:32 -0300

    criando annotation de validação customizada

--- vendas/src/main/java/io/github/VagnerBelfort/rest/dto/PedidoDTO.java
+++ vendas/src/main/java/io/github/VagnerBelfort/rest/dto/PedidoDTO.java
@@ -1,6 +1,7 @@
 package io.github.VagnerBelfort.rest.dto;
 
 
+import io.github.VagnerBelfort.validation.NotEmptyList;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,8 +17,11 @@ public class PedidoDTO {
 
     @NotNull(message = "Informe o código do cliente.")
     private Integer cliente;
+
     @NotNull(message = "Campo total do pedido é obrigatório.")
     private BigDecimal total;
+
+    @NotEmptyList(message = "Pedido não pode ser realizado sem itens.")
     private List<ItemPedidoDTO> items;
 
 }
---
+++ vendas/src/main/java/io/github/VagnerBelfort/validation/constraintvalidation/NotEmptyListValidator.java
@@ -0,0 +1,20 @@
+package io.github.VagnerBelfort.validation.constraintvalidation;
+
+import io.github.VagnerBelfort.validation.NotEmptyList;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.List;
+
+public class NotEmptyListValidator implements ConstraintValidator<NotEmptyList, List> {
+
+    @Override
+    public boolean isValid(List list, ConstraintValidatorContext constraintValidatorContext) {
+        return list != null && !list.isEmpty();
+    }
+
+    @Override
+    public void initialize(NotEmptyList constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+}
